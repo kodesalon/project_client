4c048c56ffc100b7fecae18dde4fe5bfc4ad1dec
yaag19 <m.sun.1290@gmail.com>
2021-05-20 20:24:07 +0900

    feat: 메인 페이지 - 게시판 전제조회 부분  화면 구성 및 scss

---
+++ src/components/BoardList.tsx
@@ -0,0 +1,19 @@
+import Borad from '../components/Board';
+const BoardList = () => {
+  return (
+    <>
+      <Borad />
+      <Borad />
+      <Borad />
+      <Borad />
+      <Borad />
+      <Borad />
+      <Borad />
+      <Borad />
+      <Borad />
+      <Borad />
+    </>
+  );
+};
+
+export default BoardList;
