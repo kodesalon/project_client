9643066914ff169f7423a3ff0113a09b48b81fe0
yaag19 <m.sun.1290@gmail.com>
2021-05-20 20:19:27 +0900

    feat: 테스트를 위한 /test route 추가

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Intro from './pages/Intro';
 import Main from './pages/Main';
 import MyPage from './pages/MyPage';
-
+import SignUp from './components/SignUp';
 const Routes = () => {
   return (
     <Router>
@@ -11,6 +11,7 @@ const Routes = () => {
         <Route exact path='/' component={Intro} />
         <Route path='/main' component={Main} />
         <Route path='/myPage' component={MyPage} />
+        <Route path='/test' component={SignUp} />
       </Switch>
     </Router>
   );
