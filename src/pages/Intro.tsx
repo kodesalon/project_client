1d73d9ab30feb6c83194e08395007d55119b3347
yaag19 <m.sun.1290@gmail.com>
2021-05-20 20:20:36 +0900

    feat: 시작 페이지 화면 구성

---
+++ src/pages/Intro.tsx
@@ -0,0 +1,19 @@
+import Login from '../components/Login';
+import '../styles/Intro.scss';
+
+const Intro = () => {
+  return (
+    <>
+      <section className='intro_img'></section>
+      <section className='intro_content'>
+        <Login />
+        <span className='intro_signup'>
+          회원이 아니신가요?{' '}
+          <u className='intro_btn-signup'>지금 가입하세요!</u>
+        </span>
+      </section>
+    </>
+  );
+};
+
+export default Intro;
