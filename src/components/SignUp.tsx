2c1a5032d85ff3221c6eb2a50ecea16e5cec8a05
yaag19 <m.sun.1290@gmail.com>
2021-05-23 21:31:57 +0900

    modify: className 추가

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,31 +1,35 @@
-import '../styles/SignUp.scss';
-
 const SignUp = () => {
   return (
     <section className='signup_container'>
       <div className='signup_content'>
         <div className='signup_title'>회 원 가 입</div>
         <input className='signup_input' placeholder='아이디' type='text' />
-        <span> 아이디는 8글자 이상이어야 합니다.</span>
+        <span className='span_constraints'>
+          아이디는 8글자 이상이어야 합니다.
+        </span>
         <input
           className='signup_input'
           placeholder='비밀번호'
           type='password'
         />
-        <span> 비밀번호는 8글자 이상이어야 합니다.</span>
+        <span className='span_constraints'>
+          비밀번호는 8글자 이상이어야 합니다.
+        </span>
 
         <input
           className='signup_input'
           placeholder='비밀번호 확인'
           type='password'
         />
-        <span> 비밀번호가 일치하지 않습니다.</span>
+        <span className='span_constraints'> 비밀번호가 일치하지 않습니다.</span>
 
         <input className='signup_input' placeholder='이름' type='text' />
-        <span> 이름은 8글자 이상이어야 합니다.</span>
+        <span className='span_constraints'>
+          이름은 8글자 이상이어야 합니다.
+        </span>
 
         <input className='signup_input' placeholder='이메일' type='email' />
-        <span>이메일이 형식에 맞아야 합니다</span>
+        <span className='span_constraints'>이메일이 형식에 맞아야 합니다</span>
 
         <div className='signup_phoneNum'>
           <input
