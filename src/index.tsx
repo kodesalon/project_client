5fe6ce2cead481e97b59b887617b2c04002e3384
yaag19 <m.sun.1290@gmail.com>
2021-05-23 21:19:49 +0900

    feat: index 파일에서 main.scss import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './sass/main.scss';
+
 import Routes from './Routes';
 
 import reportWebVitals from './reportWebVitals';
