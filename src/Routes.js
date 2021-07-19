9317c42b789927d8af164dc91850d91d1e27cf53
yaag19 <m.sun.1290@gmail.com>
2021-07-19 19:49:29 +0900

    feat: UserInfoContextProvider 적용

--- src/Routes.js
+++ src/Routes.js
@@ -1,21 +1,34 @@
-import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Intro from './pages/Intro';
-import Home from './pages/Home';
-import MyPage from './pages/MyPage';
-import SignUp from './components/SignUp';
-
-import BoardCreator from './pages/BoardCreator';
+import Intro from 'pages/Intro';
+import Home from 'pages/Home';
+import MyPage from 'pages/MyPage';
+import SignUp from 'components/SignUp';
+import UserInfoContextProvider from 'contexts/UserInfoContextProvider';
+import BoardCreator from 'pages/BoardCreator';
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path='/' component={Intro} />
-        <Route path='/home' component={Home} />
+        <Route exact path='/'>
+          <UserInfoContextProvider>
+            <Intro />
+          </UserInfoContextProvider>
+        </Route>
+        <Route path='/home'>
+          <UserInfoContextProvider>
+            <Home />
+          </UserInfoContextProvider>
+        </Route>
         <Route path='/write' component={BoardCreator} />
         <Route path='/mypage' component={MyPage} />
         <Route path='/signup' component={SignUp} />
+
+        {/* <Route path='/signup'>
+          <SignUpContextProvider>
+            <SignUp />
+          </SignUpContextProvider>
+        </Route> */}
       </Switch>
     </Router>
   );
