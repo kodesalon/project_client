a21564ed7b329d465b5d626713c184804b446814
yaag19 <m.sun.1290@gmail.com>
2021-07-19 19:48:47 +0900

    feat: login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,65 @@
+import { useContext, useEffect, useState } from 'react';
+
+import * as memberAPI from 'apis/member';
+import Modal from 'common/Modal';
+import { UserInfoContext } from 'contexts/UserInfoContextProvider';
+import { useHistory } from 'react-router-dom';
+
 const Login = () => {
+  const [alias, setAlias] = useState('');
+  const [password, setPassword] = useState('');
+  const [serverMessage, setServerMessage] = useState('');
+  const { memberId, setMemberId } = useContext(UserInfoContext);
+  const history = useHistory();
+
+  const submitLogin = (e) => {
+    e.preventDefault();
+    memberAPI.postMemberLogin({ alias, password }).then((res) => {
+      console.log(res);
+      if (res) {
+        setServerMessage(res);
+      }
+      if (res === 'success') {
+        // setMemberId(res.data.memberId); - localstorage에 저장할꺼니까 필요없나?
+        //  localStorage.setItem('id', res.data.memberId);
+        history.push('/home');
+      }
+    });
+  };
+  const handleOnClose = () => {
+    setServerMessage('');
+  };
+  useEffect(() => {
+    console.log(memberId);
+  }, []);
   return (
-    <div className='login_content'>
+    <form className='login_content' onSubmit={submitLogin}>
       <span className='login_title'>로 그 인</span>
-      <input className='login_input' placeholder='아이디' type='text' />
-      <input className='login_input' placeholder='비밀번호' type='password' />
-      <button className='login_btn-signin'>LOGIN</button>
-    </div>
+      <input
+        className='login_input'
+        placeholder='아이디'
+        name='alias'
+        type='text'
+        value={alias}
+        onChange={(e) => setAlias(e.target.value)}
+        // changeInput={handleOnChange}
+      />
+      <input
+        className='login_input'
+        placeholder='비밀번호'
+        name='pwd'
+        type='text'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        // changeInput={handleOnChange}
+      />
+      <button className='login_btn-signin' type='submit'>
+        LOGIN
+      </button>
+      {serverMessage && (
+        <Modal content={serverMessage} handleOnClose={handleOnClose} />
+      )}
+    </form>
   );
 };
 export default Login;
